351bc89e6a20fcdef05e8717870ba60427f3948a
igrvlhlb <igor.vilhalba@gmail.com>
2020-09-27 14:19:07 -0300

    Muda a estrategia utilizada com as filas. Antes elas armazenavam o indice dos numeros negativos lidos; agora ela armazena cada um dos numeros (dessa forma e mais simples de manter a ordem original). Tambem corrige erros nos prototipos de funcoes em bbx-flags.h

--- src/bbx-queue.c
+++ src/bbx-queue.c
@@ -1,37 +1,41 @@
-#include <string.h>
 #include "bbx-queue.h"
+#include <stdio.h>
 
-struct
-bbx_queue{
-	int head, tail;
-	int idx[Q_SIZE]; 
-};
-
-inline int bbx_peek(struct bbx_queue *q){
+inline char *bbx_peek(struct bbx_queue *q){
 	if( q->head != q->tail ){
-		return q->idx[q->head];
+		return q->args[q->head];
 	}
 	else return Q_ERR; //vazia
 }
 
-inline int bbx_dequeue(struct bbx_queue *q){
+inline char *bbx_dequeue(struct bbx_queue *q){
 	if( q->head != q->tail ){
-		int res = q->idx[q->head];
-		q->head= (q->head + 1) % Q_SIZE;
+		char *res = q->args[q->head];
+		q->head = (q->head + 1) % Q_SIZE;
 		return res;
 	} else return Q_ERR; //vazia
 }
 
-inline int bbx_enqueue(struct bbx_queue *q){
+inline char *bbx_enqueue(struct bbx_queue *q, char *str){
 	if( q->head != q->tail ){
+		q->args[q->tail] = str; //adiciona ponteiro para string
 		q->tail = (q->tail + 1) % Q_SIZE;
 		if( q->head == -1 ) q->head+=1; //lista populada; agora head = 0, tail = 1
-		return 0;
+		return str;
 	} else return Q_ERR;
 }
 
 inline void bbx_queue_init(struct bbx_queue *q){
 	q->head=-1;
 	q->tail=0;
-	memset(q->idx, 0, sizeof(int)*Q_SIZE);
+}
+
+inline void bbx_queue_stats(struct bbx_queue *q){
+	fprintf(stderr, "head:\t%d\n"
+			"tail:\t%d\n"
+			"*head:\t%s\n"
+			"*tail:\t%s\n",
+			q->head, q->tail,
+			q->args[q->head],
+			q->args[q->tail]);
 }
--- src/bbx-queue.h
+++ src/bbx-queue.h
@@ -1,9 +1,21 @@
 #define Q_SIZE 512
-#define Q_ERR (((int) ~0) >> 1)
+#define Q_ERR 0 //null
 
-struct bbx_queue;
+/* 
+ * Esta struct e uma fila que guardara as strings a
+ * serem lidas. Isto porque getopt() nao diferenciara
+ * opcoes (-[a-zA-Z]) de numeros negativos (-[0-9]).
+ * Por esse motivo, as strings serao lidas e enfileiradas
+ * antes de getopt() lê-las, ja que reordena os elementos
+ */
+struct
+bbx_queue{
+	int head, tail;
+	char *args[Q_SIZE]; 
+};
 
-inline int bbx_peek(struct bbx_queue *q);
-inline int bbx_dequeue(struct bbx_queue *q);
-inline int bbx_enqueue(struct bbx_queue *q);
-inline void bbx_queue_init(struct bbx_queue *q);
+char *bbx_peek(struct bbx_queue *q);
+char *bbx_dequeue(struct bbx_queue *q);
+char *bbx_enqueue(struct bbx_queue *q, char *str);
+void bbx_queue_init(struct bbx_queue *q);
+void bbx_queue_stats(struct bbx_queue *q);
